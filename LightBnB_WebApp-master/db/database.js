e98f3c3aff8d3f2150e7520c1e54654a647bba60
robotbeepboop <127355581+robotbeepboop@users.noreply.github.com>
2023-05-04 12:13:34 -0700

    refactor getallreservations

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -70,7 +70,19 @@ const addUser = function (user) {
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`SELECT *
+    FROM reservations
+    JOIN properties ON properties.id = property_id
+    WHERE reservations.guest_id = $1
+    LIMIT $2`, [guest_id, limit])
+    .then((result) => {
+      console.log(result.rows);
+      return result.rows;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /// Properties
